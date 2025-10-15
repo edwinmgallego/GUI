7b532695c1a4a103c7d76a73fdedf9cba9b363e1
edwinmgallego <ing.edwin.gallego@gmail.com>
2025-10-15 09:10:35 -0500

    creando unavamos a crear  un  GUI

---
+++ src/main/java/com/uao/swinggui/SwingGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.swinggui;
+
+/**
+ *
+ * @author emgallego
+ */
+public class SwingGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
